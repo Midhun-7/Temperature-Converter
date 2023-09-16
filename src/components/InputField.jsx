198f14b0496020597efd00d70b075e015ed79e7c
Midhun Muraleedharan <73743937+Midhun-7@users.noreply.github.com>
2023-09-17 00:28:18 +0530

    Add CSS

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -3,10 +3,17 @@ import React from "react";
 
 const InputField = ({ label, value, onChange }) => {
   return (
-    <div className="input-field">
-      <label>{label}</label>
-      <input type="number" value={value} onChange={onChange} />
+  <div class="row">
+    <div class="col-sm-6 mb-3 mb-sm-0">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">{label}</h5>
+          <input type="number" value={value} onChange={onChange} />
+          <a href="#" class="btn btn-primary">Convert</a>
+        </div>
     </div>
+  </div>
+</div>
   );
 };
 
